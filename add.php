829008d1a0cf56d330be2a18736a4f4b96e4ad25
Krzysztof Kolodenny <dzidolos@gmail.com>
2019-09-07 10:35:01 +0200

    New functionality

--- add.php
+++ add.php
@@ -1,27 +1,59 @@
 <?php
-include('base.php');
-
+include( 'session.php' );
 if( isSet( $_POST['nazwa'] ) ) {
-    $id = isset($_POST['$id'])? intval($_POST['id']): 0;
-    if($id>0){
-    $sth = $pdo->prepare('UPDATE `samochod` SET `id`=:id,`nazwa`=:nazwa,`rok_produkcji`=:rok_produkcji,`cena`=:cena, WHERE id=:id');
+    $id = isSet( $_POST['id'] ) ? intval( $_POST['id'] ) : 0;
+    $fileName = 0;
+    if( isSet( $_FILES['cover']['error'] ) && $_FILES['cover']['error'] == 0 ) {
+        require( "vendor/autoload.php" );
+        $uid = uniqid();
+        $ext = pathinfo( $_FILES['cover']['name'], PATHINFO_EXTENSION );
+        $fileName = 'cover_' . $uid . '.' . $ext;
+        $fileNameOrg = 'org_' . $uid . '.' . $ext;
+        $imagine = new Imagine\Gd\Imagine();
+        $size    = new Imagine\Image\Box(200, 200);
+        //$mode    = Imagine\Image\ImageInterface::THUMBNAIL_INSET;
+        $mode    = Imagine\Image\ImageInterface::THUMBNAIL_OUTBOUND;
+        $imagine->open( $_FILES['cover']['tmp_name'] )
+            ->thumbnail( $size, $mode )
+            ->save( __DIR__ . '/img/' . $fileName );
+        move_uploaded_file( $_FILES['cover']['tmp_name'], __DIR__ . '/img/' . $fileNameOrg );
     }
-    $sth = $pdo->prepare('INSERT INTO `samochod`(`nazwa`, `rok_produkcji`, `cena`) VALUES (:nazwa,:rok_produkcji,:cena)');
-    $sth->bindParam(':nazwa', $_POST['$nazwa']);
-    $sth->bindParam(':rok_produkcji', $_POST['$rok_produkcji']);
-    $sth->bindParam(':cena', $_POST['$cena']);
+    if( $id > 0 ) {
+        if( $fileName ) {
+            $sth = $pdo->prepare( 'UPDATE `komis` SET `nazwa`=:nazwa,`rok_produkcji`=:rok_produkcji,`cena`=:cena,cover=:cover WHERE id = :id' );
+            $sth->bindParam( ':cover', $fileName );
+            $sthCov = $pdo->prepare( 'SELECT cover FROM komis WHERE id = :id' );
+            $sthCov->bindParam( ':id', $id );
+            $sthCov->execute();
+            $cover = $sthCov->fetch()['cover'];
+            if( $cover ) {
+                unlink( __DIR__ . '/img/' . $cover );
+                unlink( __DIR__ . '/img/' . str_replace( 'cover_', 'org_', $cover ) );
+            }
+        } else {
+            $sth = $pdo->prepare( 'UPDATE `komis` SET `nazwa`=:nazwa,`rok_produkcji`=:rok_produkcji,`cena`=:cena,WHERE id = :id' );
+        }
+        $sth->bindParam( ':id', $id );
+    } else {
+        $sth = $pdo->prepare( 'INSERT INTO `komis`(`nazwa`, `rok_produkcji`, `cena`) VALUES ( :nazwa, :rok_produkcji, :cena)' );
+        if( $fileName ) {
+            $sth->bindParam( ':cover', $fileName );
+        }
+    }
+    $sth->bindParam( ':nazwa', $_POST['nazwa'] );
+    $sth->bindParam( ':rok_produkcji', $_POST['rok_produkcji'] );
+    $sth->bindParam( ':cena', $_POST['cena'] );
     $sth->execute();
-    header('location: loop.php');
+    header( 'location: loop.php' );
 }
- $idGet=isSet($_GET['id'])  ? intval($_GET['id']): 0;
-if($idGet>0){
-    $sth=$pdo->prepare('SELECT * FROM `komis` WHERE id=:id');
-    $sth->bindParam(':id',$idGet);
+$idGet = isSet( $_GET['id'] ) ? intval( $_GET['id'] ) : 0;
+if( $idGet > 0 ) {
+    $sth = $pdo->prepare( 'SELECT * FROM komis WHERE id = :id' );
+    $sth->bindParam( ':id', $idGet );
     $sth->execute();
-    $result=$sth->fetch();
+    $result = $sth->fetch();
 }
 ?>
-
 <form method="post" action="add.php">
     Nazwa:<input type="text" name="nazwa"> <br> <br>
     Rok produkcji: <input type="number" name="rok_produkcji"> <br> <br>
